42ad90fe619fa22f592518f0605fc33217d3743f
lezzzgooooooo <hahahaobfuscated@gmail.com>
2026-06-15 22:37:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const cors=require("cors");
 const app=express();
 
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET","POST","PATCH","DELETE","OPTIONS"],
+    allowedHeaders: ["Content-Type","Authorization"]
+}));
+
+app.options("*", cors());
 app.use(express.json());
 
 
